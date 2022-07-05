37932c1d565ee6e649a7e7d094bc4e08f6258f8e
Büşra Hekimoğlu <45571159+busrahekim@users.noreply.github.com>
2022-07-05 11:46:45 +0300

    Add project files.

---
+++ formlarArasiGecis/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace formlarArasiGecis
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "sayfa 2\'ye git";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(342, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "SAYFA1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+    }
+}
